74e23f511af1aad1f7a55c989e5db58e74d17c1c
Rose Maina <rosewairimu27@gmail.com>
2022-07-27 14:05:58 +0300

    Removed my-app

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Home from "./components/Home";
 import Navbar from "./components/Navbar";
+import About from "./components/About";
 
 function App() {
   return (
     <Router>
       <div>
+        <Home />
         <Navbar/>
       </div>
+
+      <Routes>
+        <Route exact path="/" element={<About/>}></Route>
+      </Routes>
 
     </Router>
    
