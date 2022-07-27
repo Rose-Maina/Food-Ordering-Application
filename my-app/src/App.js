2f1ea3045762054938ffc8375fc6798d43818577
Rose Maina <rosewairimu27@gmail.com>
2022-07-27 12:10:05 +0300

    Updated Page files

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,24 @@
-import React from "react";
-// import Menu from "./Menu";
+import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from './pages';
+import About from './pages/about';
+import Contact from './pages/contact';
+import SignUp from './pages/signup';
+import SignIn from './pages/signin';
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
