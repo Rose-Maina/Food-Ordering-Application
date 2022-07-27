439289d6f7d811b71d46b4893735ebe56c1b22f0
Rose Maina <rosewairimu27@gmail.com>
2022-07-27 15:35:03 +0300

    Update Readme

--- README.md
+++
@@ -1,2 +0,0 @@
-# Food-Ordering-Application
-A web application that will allow users to make purchases for different kind of foods
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,20 +1,22 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 
 const Navbar = () => {
     return (
         <>
           <nav className="navbar bg-light display-block ">
-            <div className="container-fluid">
-                {/* <ul className="navbar-nav"> */}
-                        <Link className="nav-link" to="/">Home</Link>
+            
+            {/* <div className="container-fluid"> */}
+                        {/* <Link className="nav-link" to="/">Home</Link>
                         <Link className="nav-link" to="/about">About</Link>
                         <Link className="nav-link" to="/contact">Contact</Link>
                         <Link className="nav-link" to="/signin">Signin</Link>
-                        <Link className="nav-link" to="/signup">Signup</Link>
-                {/* </ul> */}
-            </div>
+                        <Link className="nav-link" to="/signup">Signup</Link> */}
+            {/* </div> */}
+            <NavLink>
+                
+            </NavLink>
         </nav>
         </>
     );
