65be261bfc0ade20596be8b211ffbcbf2dfde919
Rose Maina <rosewairimu27@gmail.com>
2022-07-29 08:44:02 +0300

    Included the Shopping Cart components

--- src/components/MenuCard.js
+++ src/components/MenuCard.js
@@ -5,13 +5,13 @@ import { Link } from "react-router-dom";
 export default function MenuCard({mealName, mealImage, mealPrice, mealId}){
 
     return (
-        <div className="card" style={{width: 18 + "rem"}}>
+        <div className="card" style={{width: 18 + "rem"}} >
             <img src={mealImage} className="card-img-top" alt={mealImage}/>
             <div className="card-body">
                 <h5 className="card-title">{mealName}</h5>
                 <p className="card-price">{mealPrice}</p>
                 {/* <a href="#" class="btn btn-primary">Add to Cart</a> */}
-                <Link to={"/meal/"+mealId} className="btn btn-primary">Description</Link>
+                <Link to={"/meal/"+mealId} className="btn btn-primary">Add to Cart</Link>
             </div>
         </div>
     )
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,12 +4,16 @@ import { Link } from "react-router-dom";
 const Navbar = () => {
     return (
         <>
-          <nav className="navbar bg-light display-block ">
+          <nav className="navbar bg-light display-block">
             <div className="container-fluid">
                         <Link className="nav-link" to="/">Home</Link>
                         <Link className="nav-link" to="/about">About</Link>
                         <Link className="nav-link" to="/contact">Contact</Link>
                         <Link className="nav-link" to="/signup">Signup</Link> 
+                        <form className="form-inline">
+                        <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"/>
+                        <button className="btn btn-sm btn-outline-secondary" type="button">Search</button>                        
+                        </form>
             </div>  
         </nav>
         </>
