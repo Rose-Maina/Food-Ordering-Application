8cdb6ed529fb661f67e825dd39d9fa4437ea1f3f
Rose Maina <rosewairimu27@gmail.com>
2022-07-30 15:42:01 +0300

    Updated App.js

--- src/App.js
+++ src/App.js
@@ -1,31 +1,27 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 import Home from "./components/Home";
-import Navbar from "./components/Navbar";
-import About from "./components/About";
-import Contact from "./components/Contact";
+import Navbar from "./components/Navbar"
+import ShoppingList from "./components/ShoppingList";
 import Signup from "./components/Signup";
-import Search from "./components/Search";
-import MenuDetails from "./components/MenuDetails";
-import "./App.css";
+import "./App.css"
 
 
 function App() {
   return (
+    <div className="App">
     <Router>
         <div>
         <h1>Yummy Treats</h1>
         <Navbar />
         <Routes>
         <Route exact path="/" element={<Home/>}></Route>
-        <Route exact path="/about" element={<About/>}></Route>
-        <Route exact path="/Contact" element={<Contact/>}></Route>
         <Route exact path="/Signup" element={<Signup/>}></Route>
-        <Route exact path="/Search" element={<Search/>}></Route>
-        <Route exact path="/meal/:mealId" element={<MenuDetails/>}></Route>
+        <Route exact path="/Shoppinglist" element={<ShoppingList/>}></Route>
         </Routes>
         </div>
     </Router>
+    </div>
    
   );
 }
